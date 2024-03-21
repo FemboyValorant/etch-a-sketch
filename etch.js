a84b56be751cdf5bc1df0dd964f5d2ecd5b7af3e
FemboyValorant <matt.sant143@gmail.com>
2024-03-21 03:01:40 -0700

    Implement number input for grid size

--- etch.js
+++ etch.js
@@ -1,21 +1,32 @@
-let boxRowLength = 16; // variable N for use in N x N grid
-let boxSize = 1000/boxRowLength; // Size per box
+
 
 const container = document.querySelector('#container');
+const input = document.querySelector('#boxNumber');
+const button = document.querySelector('#boxConfirm');
 
-// Loop N x N times
-for (let i = 0; i < boxRowLength; i++){
-    for (let j = 0; j < boxRowLength; j++){
-        const box = document.createElement('div');
-        box.classList.add("redbox");
+button.addEventListener('click', () => {
+    container.innerHTML = ''; // Clear box container
+    let boxRowLength = input.value; // variable N for use in N x N grid
+    let boxSize = 1000/boxRowLength; // Size per box
 
-        box.style.width = boxSize + "px";
-        box.style.height = boxSize + "px";
+    if (boxRowLength <= 100){
+        // Generate NxN grid
+        for (let i = 0; i < boxRowLength; i++){
+            for (let j = 0; j < boxRowLength; j++){
+                const box = document.createElement('div');
+                box.classList.add("redbox");
+    
+                box.style.width = boxSize + "px";
+                box.style.height = boxSize + "px";
+    
+                box.addEventListener("mouseover", () =>
+                    box.style.backgroundColor = "#" + randomColor
+                );
+    
+                container.appendChild(box);
+            }
+        }
+    } 
+    // Loop N x N times
 
-        box.addEventListener("mouseover", () =>
-            box.style.backgroundColor = "red"
-        );
-
-        container.appendChild(box);
-    }
-}
+})
